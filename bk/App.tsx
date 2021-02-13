b68203c9841fba5a664992d6148ae889313db4a6
akky25 <ry.akita1991@gmail.com>
2021-02-13 19:31:09 +0900

    init commit

---
+++ bk/App.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import CalenderBoard from "./components/CalendarBoard/CalendarBoard";
+
+const App: FC = () => (
+  <div>
+    <CalenderBoard />
+  </div>
+);
+
+export default App;
