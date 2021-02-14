82443efc59fb2a7cb6775b4012f30a9faa47a764
akky25 <ry.akita1991@gmail.com>
2021-02-14 22:23:39 +0900

    Change the layout for today

--- front/src/components/CalendarElement/CalendarElement.tsx
+++ front/src/components/CalendarElement/CalendarElement.tsx
@@ -8,17 +8,25 @@ type Props = {
   day: dayjs.Dayjs;
 };
 
-const CalendarElement: FC<Props> = ({ day }) => (
-  <div className={styles.element}>
-    <Typography
-      className={styles.date}
-      align="center"
-      variant="caption"
-      component="div"
-    >
-      {day.format(day.date() === 1 ? "M月D日" : "D")}
-    </Typography>
-  </div>
-);
+const CalendarElement: FC<Props> = ({ day }) => {
+  const today = dayjs();
+  const compareFormat = "YYYYMMDD";
+  const isToday = day.format(compareFormat) === today.format(compareFormat);
+
+  return (
+    <div className={styles.element}>
+      <Typography
+        className={styles.date}
+        align="center"
+        variant="caption"
+        component="div"
+      >
+        <span className={isToday ? styles.today : ""}>
+          {day.format(day.date() === 1 ? "M月D日" : "D")}
+        </span>
+      </Typography>
+    </div>
+  );
+};
 
 export default CalendarElement;
